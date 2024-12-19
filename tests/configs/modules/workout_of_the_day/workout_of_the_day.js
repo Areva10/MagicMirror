00f058f3db4c9b73461944bfa1bb57df3910dd98
areva10 <alan.arevalo.b@gmail.com>
2024-12-19 15:35:26 -0800

    add tests

---
+++ tests/configs/modules/workout_of_the_day/workout_of_the_day.js
@@ -0,0 +1,13 @@
+let config = {
+	modules: [
+		{
+			module: "workout_of_the_day",
+			position: "bottom_bar"
+		}
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
